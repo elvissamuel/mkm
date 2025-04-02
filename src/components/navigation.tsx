53d5bf1846ede0e371d8bef290e74c4fee8a7e54
Elvis Tehinse <elvissamuel8@gmail.com>
2025-04-02 02:37:41 +0100

    add styling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,7 +6,7 @@ export default function Navigation() {
   return (
     <nav className="border-b border-gray-200 bg-white px-4 py-4">
       <div className="container mx-auto flex items-center justify-between">
-        <div className="border bg-slate-950 w-16 h-16">
+        <div className="border bg-slate-900 w-16 h-16">
           <Image src="/mkm-logo2.png" alt="mkm-logo" className="object-contain w-full h-full" width={100} height={100} />
         </div>
         <div className="flex items-center gap-8 text-black">
